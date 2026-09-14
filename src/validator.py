4661eb47b02dd2f99e8ca3f040f633a29fda8aa7
GautamMenon <officially.gm11@gmail.com>
2026-09-14 17:46:53 +0530

    feat: add pdf validator with unit tests

---
+++ src/validator.py
@@ -0,0 +1,9 @@
+def validate(pdf_bytes: bytes) -> None:
+    if not pdf_bytes:
+        raise ValueError("The file is empty!")
+
+    if len(pdf_bytes) > 1024 * 1024 * 10:
+        raise ValueError("File size exceeds 10MB!")
+
+    if pdf_bytes[:5] != b"%PDF-":
+        raise ValueError("The file isn't a PDF.")
